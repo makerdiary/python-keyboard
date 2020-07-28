579e0cd4d381bcf7e1b90be5e1bfcfacd879516c
Yihui Xiong <yihui.xiong@hotmail.com>
2020-07-28 16:57:09 +0800

    add log option, usb and ble are switched automatically

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -1,6 +1,7 @@
 
 import array
 import time
+import supervisor
 import usb_hid
 
 import adafruit_ble
@@ -16,10 +17,7 @@ from .action_code import *
 ___ = TRANSPARENT
 BOOT = BOOTLOADER
 L1 = LAYER_TAP(1)
-L2 = LAYER_TAP(2)
 L2D = LAYER_TAP(2, D)
-L3 = LAYER_TAP(3)
-T3 = LAYER_TAP_TOGGLE(3)
 
 # Semicolon & Ctrl
 SCC = MODS_TAP(MODS(RCTRL), ';')
@@ -52,15 +50,6 @@ KEYMAP = (
         ___, ___, ___, ___, ___, ___,PGDN, ___, ___, ___, ___,           ___,
         ___, ___, ___,                ___,               ___, ___, ___,  ___
     ),
-
-    # layer 3
-    (
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,      ___,
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,     UP,
-        ___, ___, ___,                ___,               ___,LEFT,DOWN,RIGHT
-    ),
 )
 
 
@@ -72,18 +61,20 @@ def reset_into_bootloader():
 
 
 class Keyboard:
-    keymap = KEYMAP
     Matrix = Matrix
     coords = COORDS
 
-    def __init__(self):
+    def __init__(self, keymap=KEYMAP, pairs=(), verbose=True):
+        self.keymap = KEYMAP
+        self.pairs = pairs
+        self.verbose = verbose
+        self.pair_keys = set()
         self.layer_mask = 1
 
     def setup(self):
         convert = lambda a: array.array('H', (get_action_code(k) for k in a))
         self.actonmap = tuple(convert(layer) for layer in self.keymap)
 
-        self.pair_keys = set()
         for pair in self.pairs:
             for key in pair:
                 self.pair_keys.add(key)
@@ -99,7 +90,13 @@ class Keyboard:
                 return code
         return 0
 
+    def log(self, *args):
+        if self.verbose:
+            print(*args)
+
     def run(self):
+        log = self.log
+
         hid = HIDService()
         advertisement = ProvideServicesAdvertisement(hid)
         advertisement.appearance = 961
@@ -113,31 +110,37 @@ class Keyboard:
         ble_keyboard = _Keyboard(hid.devices)
         usb_keyboard = _Keyboard(usb_hid.devices)
 
+        runtime = supervisor.runtime
+
         def send(*code):
-            usb_keyboard.press(*code)
-            usb_keyboard.release(*code)
-            if ble.connected:
+            if runtime.serial_connected:
+                usb_keyboard.press(*code)
+                usb_keyboard.release(*code)
+            elif ble.connected:
                 ble.advertising = False
                 ble_keyboard.press(*code)
                 ble_keyboard.release(*code)
 
         def press(*code):
-            usb_keyboard.press(*code)
-            if ble.connected:
+            if runtime.serial_connected:
+                usb_keyboard.press(*code)
+            elif ble.connected:
                 ble.advertising = False
                 ble_keyboard.press(*code)
 
         def release(*code):
-            usb_keyboard.release(*code)
-            if ble.connected:
+            if runtime.serial_connected:
+                usb_keyboard.release(*code)
+            elif ble.connected:
                 ble.advertising = False
                 ble_keyboard.release(*code)
 
         self.setup()
         matrix = self.Matrix()
+        ms = matrix.ms
         keys = [0] * matrix.keys
         while True:
-            n = matrix.scan()
+            n = matrix.wait(10)
             if n == 0:
                 continue
 
@@ -145,7 +148,7 @@ class Keyboard:
             if n == 1:
                 key = matrix.view(0)
                 if key < 0x80 and key in self.pair_keys:
-                    n = matrix.wait(10 - matrix.ms(matrix.time() - matrix.get_keydown_time(key)))
+                    n = matrix.wait(10 - ms(matrix.time() - matrix.get_keydown_time(key)))
 
             if n >= 2:
                 pair = {matrix.view(0), matrix.view(1)}
@@ -153,9 +156,9 @@ class Keyboard:
                     pair_index = self.pairs.index(pair)
                     key1 = matrix.get()
                     key2 = matrix.get()
-                        
-                    dt = matrix.get_keydown_time(key2) - matrix.get_keydown_time(key1)
-                    print('pair keys {} {}, dt = {}'.format(pair_index, pair, dt))
+
+                    dt = ms(matrix.get_keydown_time(key2) - matrix.get_keydown_time(key1))
+                    log('pair keys {} {}, dt = {}'.format(pair_index, pair, dt))
                     # todo
                     for c in b'pair keys':
                         send(ASCII_TO_KEYCODE[c])
@@ -168,8 +171,9 @@ class Keyboard:
                     keys[key] = action_code
                     if action_code < 0xFF:
                         press(action_code)
-                        dt = matrix.ms(matrix.time() - matrix.get_keydown_time(key))
-                        print('{} \\ {} latency {}'.format(key, hex(action_code), dt))
+                        if self.verbose:
+                            dt = ms(matrix.time() - matrix.get_keydown_time(key))
+                            log('{} \\ {} latency {}'.format(key, hex(action_code), dt))
                     else:
                         kind = action_code >> 12
                         if kind < ACT_MODS_TAP:
@@ -181,9 +185,9 @@ class Keyboard:
                         elif kind < ACT_USAGE:
                             # MODS_TAP
                             if len(matrix) == 0:
-                                matrix.wait(500 - matrix.ms(matrix.time() - matrix.get_keydown_time(key)))
+                                matrix.wait(500 - ms(matrix.time() - matrix.get_keydown_time(key)))
                             if len(matrix) > 0 and matrix.view(0) == (key | 0x80):
-                                print('press & release quickly')
+                                log('press & release quickly')
                                 keycode = action_code & 0xFF
                                 keys[key] = keycode
                                 press(keycode)
@@ -199,9 +203,9 @@ class Keyboard:
                             keycode = action_code & 0xFF
                             if keycode != OP_TAP_TOGGLE:
                                 if len(matrix) == 0:
-                                    matrix.wait(500 - matrix.ms(matrix.time() - matrix.get_keydown_time(key)))
+                                    matrix.wait(500 - ms(matrix.time() - matrix.get_keydown_time(key)))
                                 if len(matrix) > 0 and matrix.view(0) == (key | 0x80):
-                                    print('press & release quickly')
+                                    log('press & release quickly')
                                     keys[key] = keycode
                                     press(keycode)
                                     matrix.get()
@@ -209,15 +213,15 @@ class Keyboard:
                                 else:
                                     self.layer_mask |= mask
                             else:
-                                print('toggle {}'.format(self.layer_mask))
+                                log('toggle {}'.format(self.layer_mask))
                                 self.layer_mask = (self.layer_mask & ~mask) | (mask & ~self.layer_mask)
-                            
-                            print('layers {}'.format(self.layer_mask))
+
+                            log('layers {}'.format(self.layer_mask))
                         elif action_code == BOOTLOADER:
                             reset_into_bootloader()
-                    
-                        dt = matrix.ms(matrix.time() - matrix.get_keydown_time(key))
-                        print('{} \\ {} latency {}'.format(key, hex(action_code), dt))
+
+                        dt = ms(matrix.time() - matrix.get_keydown_time(key))
+                        log('{} \\ {} latency {}'.format(key, hex(action_code), dt))
                 else:
                     action_code = keys[key]
                     if action_code < 0xFF:
@@ -240,18 +244,12 @@ class Keyboard:
                             keycode = action_code & 0xFF
                             if keycode != OP_TAP_TOGGLE:
                                 self.layer_mask &= ~(1 << layer)
-                                print('layers {}'.format(self.layer_mask))
-                    
-                    dt = matrix.ms(matrix.time() - matrix.get_keyup_time(key))
-                    print('{} / {} latency {}'.format(key, hex(action_code), dt))
+                                log('layers {}'.format(self.layer_mask))
+
+                    if self.verbose:
+                        dt = ms(matrix.time() - matrix.get_keyup_time(key))
+                        log('{} / {} latency {}'.format(key, hex(action_code), dt))
 
             if not ble.connected and not ble.advertising:
                 ble.start_advertising(advertisement)
                 ble.advertising = True
-
-
-def main():
-    kbd = Keyboard()
-    # pair key {J, K}
-    kbd.pairs = [{35, 36}]
-    kbd.run()

e35fd813e403e34d8ca4df53cc92d38b86d7a79e
Yihui Xiong <yihui.xiong@hotmail.com>
2020-09-22 00:24:54 +0800

    update code.py

--- code.py
+++ code.py
@@ -46,7 +46,7 @@ keyboard.keymap = (
     # layer 3
     (
         BT_TOGGLE,BT1,BT2, BT3,BT4,BT5,BT6,BT7, BT8, BT9, BT0, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___,___,USB_TOGGLE,___,___,___,___,___, ___,
+        RGB_MOD, ___, ___, ___, ___, ___,___,USB_TOGGLE,___,___,___,___,___, ___,
         ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,      ___,
         ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,           ___,
         ___, ___, ___,                ___,               ___, ___, ___,  ___
@@ -66,13 +66,14 @@ keyboard.keymap = (
 # Valid keys are "USB" and "BT0"-"BT9"
 # Connection not in this map will use default keymap defined above.
 keyboard.profiles = {
-    "BT1": (
+    # For example, BT8 is connected to a Mac
+    "BT8": (
         # layer 0
         (
             ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, '-', '=', BACKSPACE,
             TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, '[', ']', '|',
-            CAPS,  A,   S, L2D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
-            LSFT4, Z,   X,   C,   V, L3B,   N,   M, ',', '.', '/',         RSFT4,
+            CAPS,  A,   S,   D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
+            LSHIFT,Z,   X,   C,   V,   B,   N,   M, ',', '.', '/',        RSHIFT,
             LCTRL, LALT, LGUI,          SPACE,            MENU, RALT,  L1, RCTRL
         ),
 
@@ -84,29 +85,6 @@ keyboard.profiles = {
             ___, ___, ___, ___, ___,BOOT, ___,MACRO(1), ___, ___, ___,       ___,
             ___, ___, ___,                ___,               ___, ___, ___,  ___
         ),
-
-        # Other layers...
-    ),
-    "BT2": (
-        # layer 0
-        (
-            ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, '-', '=', BACKSPACE,
-            TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, '[', ']', '|',
-            CAPS,  A,   S, L2D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
-            LSFT4, Z,   X,   C,   V, L3B,   N,   M, ',', '.', '/',         RSFT4,
-            LCTRL, LGUI, LALT,          SPACE,            RALT, MENU,  L1, RCTRL
-        ),
-
-        # layer 1
-        (
-            '`',  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
-            ___, ___,  UP, ___, ___, ___, ___, ___, ___, ___,SUSPEND,___,___,___,
-            ___,LEFT,DOWN,RIGHT,___, ___, ___, ___, ___, ___, ___, ___,      ___,
-            ___, ___, ___, ___, ___,BOOT, ___,MACRO(2), ___, ___, ___,       ___,
-            ___, ___, ___,                ___,               ___, ___, ___,  ___
-        ),
-
-        # Other layers...
     )
 }
 
@@ -123,17 +101,11 @@ def pairs_handler(dev, n):
 keyboard.macro_handler = macro_handler
 keyboard.pairs_handler = pairs_handler
 
-# ESC   1   2   3   4   5   6   7   8   9   0   -   =  BACKSPACE
-# TAB   Q   W   E   R   T   Y   U   I   O   P   [   ]   |
-# CAPS  A   S   D   F   G   H   J   K   L   ;   "      ENTER
-#LSHIFT Z   X   C   V   B   N   M   ,   .   /         RSHIFT
-# LCTRL LGUI LALT          SPACE         RALT MENU  L1 RCTRL
-#
-#   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13,
-#   27,26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14,
-#   28,29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,     40,
-#   52,51, 50, 49, 48, 47, 46, 45, 44, 43, 42,         41,
-#   53,  54, 55,             56,           57, 58, 59, 60
+# ESC(0)    1(1)   2(2)   3(3)   4(4)   5(5)   6(6)   7(7)   8(8)   9(9)   0(10)  -(11)  =(12)  BACKSPACE(13)
+# TAB(27)   Q(26)  W(25)  E(24)  R(23)  T(22)  Y(21)  U(20)  I(19)  O(18)  P(17)  [(16)  ](15)   \(14)
+# CAPS(28)  A(29)  S(30)  D(31)  F(32)  G(33)  H(34)  J(35)  K(36)  L(37)  ;(38)  "(39)      ENTER(40)
+#LSHIFT(52) Z(51)  X(50)  C(49)  V(48)  B(47)  N(46)  M(45)  ,(44)  .(43)  /(42)            RSHIFT(41)
+# LCTRL(53)  LGUI(54)  LALT(55)               SPACE(56)          RALT(57)  MENU(58)  Fn(59)  RCTRL(60)
 
 # Pairs: J & K, U & I
 keyboard.pairs = [{35, 36}, {20, 19}]
